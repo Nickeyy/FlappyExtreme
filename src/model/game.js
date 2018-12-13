d7c99b0a9613a30bb9b9b4b8733a55a54dc8d185
Nick Durrer <nick7@hotmail.ch>
2018-12-13 09:31:13 +0100

    Auf xxamp gewechselt und hat jetzt Bird und Hintergrundbild

--- src/model/game.js
+++ src/model/game.js
@@ -2,29 +2,41 @@ var pipes = [];
 var bird;
 var bg;
 var bat;
-var y=0;
+var bgx=0;
+var canvas; 
+
 
 function preload() {
-
+    bg = loadImage('../../res/backGroundGame.jpg');
+    bat = loadImage('../../res/bird/main.png');
 }
 
 function setup(){
 
     bird = new Bird();
     pipes.push(new Pipe());
-   
-   // bat = loadImage("../../res/bird/main.png");
-    //bg = loadImage("../../res/backGroundGame.jpg");
-   
+    canvas = createCanvas(520, 923);
+    canvas.class('canvas-bg'); 
 
 }   
 
 function draw(){
-  
-    //alert('3');
-    createCanvas(700, 920);
-  
-   //background(bg);
+   
+    background(0);
+    image(bg,bgx,0,bg.width,height);
+    bgX -= pipes[0].speed * parallax;
+
+    // this handles the "infinite loop" by checking if the right
+    // edge of the image would be on the screen, if it is draw a
+    // second copy of the image right next to it
+    // once the second image gets to the 0 point, we can reset bgX to
+    // 0 and go back to drawing just one image.
+    /*if (bgX <= -bgImg.width + width) {
+      image(bgImg, bgX + bgImg.width, 0, bgImg.width, height);
+      if (bgX <= -bgImg.width) {
+        bgX = 0;
+      }
+    }*/
 
     for(var i = pipes.length-1; i >= 0; i--){
         pipes[i].show();
